014b8034d07803b4440da12dd9f327d5651edf37
Wawi <122799427+wawi-m@users.noreply.github.com>
2024-10-04 08:36:32 +0000

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,11 +11,11 @@ def pascal_triangle(n):
 
     for i in range(n):
         row = [1] * (i + 1)  # Initialize the row with 1s
-      
+
         # Calculate the values in the row
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-      
+
         triangle.append(row)  # Append the row to the triangle
-  
+
     return triangle
